2d31498773a49ebea15955e6b0115bbea117b97f
gfrnca <gabrielbfranca27@gmail.com>
2023-04-24 04:20:24 -0300

    feat: container component

---
+++ src/components/atoms/Container.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  background: var(--cinza-claro);
+  margin: 4em;
+  border-radius: 16px;
+  padding: 2em;
+  height: 100%;
+`;
